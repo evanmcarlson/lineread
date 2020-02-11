84b9285d6183d4633f985b974991e3d653e615bd
Evan Carlson <35385537+evanmcarlson@users.noreply.github.com>
2020-02-10 20:14:29 -0800

    Update lineread.c

--- lineread.c
+++ lineread.c
@@ -8,22 +8,22 @@
 ssize_t lineread(int fd, char *buf, size_t sz) {
 
 	ssize_t bytes_read = 0;
-	char *ch;
+	char ch;
 
 	while(bytes_read < sz) {
 		// read one character from file
-		int status = read(fd, ch, 1);
+		int status = read(fd, &ch, 1);
 
-		if(status != 0 && *ch != '\n') { // not EOF and not a newline
+		if(status != 0 && ch != '\n') { // not EOF and not a newline
 			strcat(buf, ch);
 			bytes_read++;
 		}
-		else if(bytes_read == 0 && *ch =='\n') { // newline bytes get their own system call
+		else if(bytes_read == 0 && ch =='\n') { // newline bytes get their own system call
 			strcat(buf, ch);
 			bytes_read++; // this should always result to 1
 			break;
 		}
-		else if(*ch == '\n') { // indicates that bytes_read > 0
+		else if(ch == '\n') { // indicates that bytes_read > 0
 			break;
 		}
 		else if(status == 0 || status == -1) { // EOF or error codes return nonzero
